4b009d34d2e75261599bb2f3bd0680ed8d0472e6
Hilson Shrestha <hilsonshrestha@gmail.com>
2024-07-15 01:34:57 -0400

    Switch to markdown format

--- app/summaries/document_reader.py
+++ app/summaries/document_reader.py
@@ -1,6 +1,7 @@
 from docx import Document
 
-class DocumentReader():
+
+class DocumentReader:
     def __init__(self, document_path):
         self.document_path = document_path
         self.document = Document(document_path)
@@ -8,47 +9,44 @@ class DocumentReader():
         self.sentences = ""
         self.markdown_content = ""
 
-    def read(self):
-        cnt_sources = 0
-        for para in self.document.paragraphs:
-            texts = list(filter(None, para.text.split(".")))
-            for text in texts:
-                cnt_sources += 1
-                self.sentence_sequence.append({"id": f"S{cnt_sources}", "text": text.strip()})
-                self.sentences += f"{text.strip()} (S{cnt_sources})."
-            self.sentences += "\n"
-
     def convert_to_markdown(self):
-        doc = Document(self.document_path)
-        markdown_lines = []
-
-        for para in doc.paragraphs:
-            if para.style.name == 'Title':
-                markdown_lines.append(f"# {para.text}")
-            elif para.style.name.startswith('Heading'):
-                level = para.style.name.split()[-1]  # Assuming styles are like 'Heading 1', 'Heading 2', etc.
-                markdown_lines.append(f"{'#' * int(level)} {para.text}")
-            else:
-                markdown_lines.append(para.text)
-            markdown_lines.append('')
-
-        self.markdown_content = "\n".join(markdown_lines)
-        return self.markdown_content
-
-    def save_markdown(self, output_path):
-        markdown_content = self.convert_to_markdown()
-        with open(output_path, 'w') as file:
-            file.write(markdown_content)
-
-    def parse_markdown(self):
-        lines = self.markdown_content.split('\n')
+        markdown_lines = {}
         sentence_sequence = []
-        cnt_sources = 0
 
-        for line in lines:
-            line = line.strip()
-            if line:
-                cnt_sources += 1
-                sentence_sequence.append({"id": f"S{cnt_sources}", "text": line})
+        idx = 0
+        for para in self.document.paragraphs:
+            if para.style.name == "Title":
+                idx += 1
+                markdown_lines[str(idx)] = f"# {para.text}"
+                sentence_sequence.append(
+                    {"id": f"{idx}", "text": para.text, "sources": []}
+                )
+            elif para.style.name.startswith("Heading"):
+                idx += 1
+                level = para.style.name.split()[
+                    -1
+                ]  # Assuming styles are like 'Heading 1', 'Heading 2', etc.
+                markdown_lines[str(idx)] = f"{'#' * int(level)} {para.text}"
+                sentence_sequence.append(
+                    {"id": f"{idx}", "text": markdown_lines[str(idx)], "sources": []}
+                )
+            else:
+                for text in para.text.split("."):
+                    idx += 1
+                    markdown_lines[str(idx)] = text.strip()
+                    if len(markdown_lines[str(idx)]) > 0:
+                        markdown_lines[str(idx)] = f"{markdown_lines[str(idx)]}."
+                    sentence_sequence.append(
+                        {
+                            "id": f"{idx}",
+                            "text": markdown_lines[str(idx)],
+                            "sources": [],
+                        }
+                    )
+            idx += 1
+            markdown_lines[idx] = ""
+            sentence_sequence.append({"id": str(idx), "text": "\n", "sources": []})
 
-        return sentence_sequence
+        self.sentence_sequence = sentence_sequence
+        self.markdown_content = markdown_lines
+        return self.markdown_content
