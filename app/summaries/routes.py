111c704150a739fa101420fb17976bcf64c961ea
BijeshShrestha <bijesh.shrestha531@gmail.com>
2024-07-20 15:00:27 -0400

    switched to GPT 4o-mini

--- app/summaries/routes.py
+++ app/summaries/routes.py
@@ -127,7 +127,7 @@ def generate():
 
     # call API
     response = openai.ChatCompletion.create(
-        model="gpt-4", messages=conversation["messages"]
+        model="gpt-4o-mini", messages=conversation["messages"]
     )
 
     # update conversation list
@@ -283,7 +283,7 @@ def generate_multiple():
 
     # call API
     response = openai.ChatCompletion.create(
-        model="gpt-4--mini", messages=conversation["messages"]
+        model="gpt-4o-mini", messages=conversation["messages"]
     )
 
     # update conversation list
